f01fcd25eb63a375c8e3e0c09edef213f494d286
scottdmccarter <44345683+scottdmccarter@users.noreply.github.com>
2019-01-13 22:21:08 +0000

    basic outline of hash functions

---
+++ rwcj49/rwcj49_q1.py
@@ -0,0 +1,61 @@
+def hash_quadratic(d):
+    """given a list d of integers returns a list of length 13
+    describing the hash table obtained when the hash function
+    h(k)=k mod 19 is applied to each integer k in d"""
+    #initialize table
+    table = ["-"]*19
+    #consider each integer k in the input
+    for k in d:
+        #if k is already in the table this is a duplicate so move to next integer in the input
+        #note this check for a duplicate is using the functionality of python rather than checking using a linear probe
+        if k in table:
+            continue
+        #apply the hash function
+        i = (6*k+1) % 19
+        #initialize count that checks whether linear probe has considered each bucket and is now full
+        count = 0
+        #while bucket is already filled
+        while table[i] != "-":
+            #move to next bucket
+            i = (i + 1) % 13
+            #increment count
+            count += 1
+            #if table is full
+            if count == 12:
+                #can return table as nothing further can be added
+                return table
+        #table[i] is empty so k can be added here
+        table[i] = k
+    #now each part of the input has been considered return the table
+    return table
+
+def hash_double(d):
+    """given a list d of integers returns a list of length 19
+    describing the hash table obtained when the hash function
+    h(k)=k mod 19 is applied to each integer k in d"""
+    #initialize table
+    table = ["-"]*19
+    #consider each integer k in the input
+    for k in d:
+        #if k is already in the table this is a duplicate so move to next integer in the input
+        #note this check for a duplicate is using the functionality of python rather than checking using a linear probe
+        if k in table:
+            continue
+        #apply the hash function
+        i = (6*k+1) % 19
+        #initialize count that checks whether linear probe has considered each bucket and is now full
+        count = 0
+        #while bucket is already filled
+        while table[i] != "-":
+            #move to next bucket
+            i = (i + i^2) % 19
+            #increment count
+            count += 1
+            #if table is full
+            if count == 18:
+                #can return table as nothing further can be added
+                return table
+        #table[i] is empty so k can be added here
+        table[i] = k
+    #now each part of the input has been considered return the table
+    return table
